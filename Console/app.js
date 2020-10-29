0e636aa984bc2b1bd37de1da75033d3bc4110fe6
Hakkı Can Şengönül <coder.9734@gmail.com>
2020-10-29 16:55:44 +0300

    Add files via upload

---
+++ Console/app.js
@@ -0,0 +1,24 @@
+console.log("merhaba javascript");
+console.log(2332);
+
+var a=30;
+console.log(a);
+
+console.log("Ahmet","Mehmet");
+
+console.log(typeof a);
+
+console.warn(  "bu bir uyarıdır");
+
+console.error("bu sayfa bulunamadı");
+
+console.clear();
+
+
+
+
+
+
+
+
+
