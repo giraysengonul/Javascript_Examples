91c36d42a85b821e9514c416f19b619bec24bd1f
Hakkı Can Şengönül <coder.9734@gmail.com>
2020-10-30 09:15:45 +0300

    Add files via upload

---
+++ Var,Let,Const/app.js
@@ -0,0 +1,41 @@
+// var 
+// var a;
+// a=20;
+// console.log(a);
+// var name="Hakkı Can";
+// var name="ali";
+// console.log(name);
+// name="can";
+// console.log(name);
+//_---------------------------
+
+//let
+// let name="Hakkı";
+// console.log(name);
+// name="can";
+// console.log(name);
+
+// let a,b;
+// a=10;
+// b=20;
+// console.log(a+b);
+//---------------------------
+
+//Const(Sabittir değişmez)
+// const name="Hakkı"
+// console.log(name);
+// name="can";
+// console.log(name);
+
+// const a;   //bu satırda değer vermemiz gerekir.
+// a=10;
+// console.log(a);
+
+const a=[1,2,3,4,5];
+console.log(a);
+
+// a=[1,2,3,4,5,6];
+// console.log(a);
+
+a.push(6);
+console.log(a);
